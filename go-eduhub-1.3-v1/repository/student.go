a855b0aac82c0a829c2a6ad4978cf905b2a68966
KrisnaAndika <127395496+KrisnaAndika@users.noreply.github.com>
2023-06-14 09:26:27 +0700

    Add files via upload

---
+++ go-eduhub-1.3-v1/repository/student.go
@@ -0,0 +1,56 @@
+package repository
+
+import (
+	"a21hc3NpZ25tZW50/model"
+
+	"gorm.io/gorm"
+)
+
+type StudentRepository interface {
+	FetchByID(id int) (*model.Student, error)
+	Store(student *model.Student) error
+	Delete(id int) error
+}
+
+type studentRepository struct {
+	db *gorm.DB
+}
+
+func NewStudentRepo(db *gorm.DB) *studentRepository {
+	return &studentRepository{db}
+}
+
+func (s *studentRepository) FetchByID(id int) (*model.Student, error) {
+	var student model.Student
+	err := s.db.Where("id = ?", id).First(&student).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &student, nil
+}
+
+func (s *studentRepository) Store(student *model.Student) error {
+	err := s.db.Create(student).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *studentRepository) Delete(id int) error {
+	// TODO: replace this
+	_, err := s.FetchByID(id)
+    if err != nil {
+        return err // Return the same error as FetchByID
+    }
+
+    // Call Delete() method of db object to delete the student with the given ID
+    err = s.db.Delete(&model.Student{}, id).Error
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
